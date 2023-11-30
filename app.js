c796d363f64854dcddaafb2c5cdda86736dc4bbf
Ahmed Karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2023-11-30 19:41:15 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-
-
-
-
-
 let getData = () => {
     return new Promise((resolve, reject) => {
 
-        fetch(`https://gnews.io/api/v4/top-headlines?category=technology&lang=en&country=pk&max=100&apikey=dc7a00a6d7bb3cca6df36f75995c8fc1`)
-        // fetch(`https://newsapi.org/v2/everything?domains=wsj.com&apiKey=2ce39e8b53304f62ba89a32618301f95`)
+        fetch(`https://newsapi.org/v2/everything?domains=wsj.com&apiKey=2ce39e8b53304f62ba89a32618301f95`)
+            // fetch(`https://newsapi.org/v2/everything?domains=wsj.com&apiKey=2ce39e8b53304f62ba89a32618301f95`)
 
             .then(data => data.json())
             .then(data => {
@@ -17,9 +12,32 @@ let getData = () => {
     })
 }
 
-
+let newsCard = document.getElementById("newsCard")
 let satDataToHtml = async () => {
     const data = await getData();
+    for (let i = 0; i < data.articles.length; i++) {
+
+        newsCard.innerHTML += `
+        <div class="col">
+        <div class="card">
+            <img src="${data.articles[i].urlToImage}"
+                class="card-img-top" alt="...">
+            <div class="card-body">
+                <h4 class="card-title">AMD Ryzen Z1 mini PC proves dynamite comes in small packages</h4>
+                <h6 class="card-title">YouTuber ETA Prime unveiled the very first AMD Ryzen Z1 mini PC prototype
+                    revealing specs, benchmarks, and more.</h6>
+                <p class="card-text">"AMD’s Ryzen Z1 APU has become increasingly popular in the handheld PC
+                    gaming market. It turns out there’s a prototype Ryzen Z1 mini PC that could offer users a
+                    compact and versatile computing solution. With the added benefit of a larger form factor
+                    t... [2504 chars]</p>
+                <p class="card-text"><small class="text-muted">published At : <span>2023-11-29 <b>12:03</b></span></small></p>
+                <a target="_blank" href="https://www.club386.com/amd-ryzen-z1-mini-pc-proves-dynamite-comes-in-small-packages/">for more details click here...</a>
+            </div>
+        </div>
+    </div>
+        `
+    }
+    console.log(data.articles[0].urlToImage)
     console.log(data)
 
 }
