e1123008dad013abdede80c2784a454818870ced
Ahmed Karim <muhammadahmedkarimnagda@gmail.com>
2023-12-01 00:58:52 +0500

    news show in html

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let getData = () => {
     return new Promise((resolve, reject) => {
 
-        fetch(`https://newsapi.org/v2/everything?domains=wsj.com&apiKey=2ce39e8b53304f62ba89a32618301f95`)
+        fetch(`https://gnews.io/api/v4/search?q=example&lang=en&country=pk&max=10&apikey=dc7a00a6d7bb3cca6df36f75995c8fc1`)
             // fetch(`https://newsapi.org/v2/everything?domains=wsj.com&apiKey=2ce39e8b53304f62ba89a32618301f95`)
 
             .then(data => data.json())
@@ -11,34 +11,31 @@ let getData = () => {
             .catch(err => reject(err))
     })
 }
-
+let newsImg = document.getElementById("newsImg")
 let newsCard = document.getElementById("newsCard")
 let satDataToHtml = async () => {
     const data = await getData();
     for (let i = 0; i < data.articles.length; i++) {
+      const imgUrl = data.articles[i].image !== null ? data.articles[i].image : "https://raysensenbach.com/wp-content/uploads/2013/04/default.jpg"
 
         newsCard.innerHTML += `
         <div class="col">
-        <div class="card">
-            <img src="${data.articles[i].urlToImage}"
+        <div class="card h-100">
+            <img id="newsImg" src="${imgUrl}"
                 class="card-img-top" alt="...">
             <div class="card-body">
-                <h4 class="card-title">AMD Ryzen Z1 mini PC proves dynamite comes in small packages</h4>
-                <h6 class="card-title">YouTuber ETA Prime unveiled the very first AMD Ryzen Z1 mini PC prototype
-                    revealing specs, benchmarks, and more.</h6>
-                <p class="card-text">"AMD’s Ryzen Z1 APU has become increasingly popular in the handheld PC
-                    gaming market. It turns out there’s a prototype Ryzen Z1 mini PC that could offer users a
-                    compact and versatile computing solution. With the added benefit of a larger form factor
-                    t... [2504 chars]</p>
-                <p class="card-text"><small class="text-muted">published At : <span>2023-11-29 <b>12:03</b></span></small></p>
-                <a target="_blank" href="https://www.club386.com/amd-ryzen-z1-mini-pc-proves-dynamite-comes-in-small-packages/">for more details click here...</a>
+                <h4 class="card-title">${data.articles[i].title}</h4>
+                <h6 class="card-title">${data.articles[i].description}</h6>
+                <p class="card-text">${data.articles[i].content}</p>
+                <p class="card-text"><small class="text-muted">published At : <span>${data.articles[i].publishedAt.slice(0,10)}<b> ${data.articles[i].publishedAt.slice(11,16)}</b></span></small></p>
+                <a target="_blank" href="${data.articles[i].url}">for more details click here...</a>
             </div>
         </div>
     </div>
         `
     }
-    console.log(data.articles[0].urlToImage)
-    console.log(data)
+    // console.log(data.articles[0].url)
+    // console.log(data)
 
 }
 satDataToHtml()
